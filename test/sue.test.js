1ae694702fab36359b95a3f84b406e335614c84f
Dominik Lessel <dominik@nuform.info>
2012-10-04 13:30:51 +0200

    Added: sendMessage(s)() + Tests

--- test/sue.test.js
+++ test/sue.test.js
@@ -1,10 +1,47 @@
-var sque = require('../lib/sque');
+// module dependencies
+var sque  = require('../lib/sque');
+var nconf = require('nconf');
+
+// load config file
+nconf.file({ file: './test/config.json' });
+
+// global var
+var queue;
 
 describe('sque', function () {
 
+  describe('getQueue()', function() {
+    it('should init without errors', function( done ) {
+      queue = sque.getQueue({
+        key      : nconf.get('AWS:Key'),
+        secret   : nconf.get('AWS:Secret'),
+        url      : nconf.get('Queue')
+      });
+      done();
+    });
+  });
+
   describe('getAttributes()', function() {
     it('should finish without errors', function( done ) {
-      
+      queue.getAttributes( done );
+    });
+  });
+
+  describe('getMessage()', function() {
+    it('should finish without errors', function( done ) {
+      queue.getMessage( done );
+    });
+  });
+
+  describe('sendMessage()', function() {
+    it('should save without errors', function( done ) {
+      queue.sendMessage({ foo : 'bar' }, done);
+    });
+  });
+
+  describe('sendMessages()', function() {
+    it('should save without errors', function( done ) {
+      queue.sendMessages([{ foo : 'bar' }, { bar : 'foo' }], done);
     });
   });
 
